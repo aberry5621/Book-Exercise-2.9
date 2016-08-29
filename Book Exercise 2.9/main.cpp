3a6a37dce57b32360a6b51700254882bbb237013
alexberry721 <alexberry721@gmail.com>
2016-08-29 14:28:02 -0700

    Init commit

--- Book Exercise 2.9/main.cpp
+++ Book Exercise 2.9/main.cpp
@@ -1,15 +1,36 @@
 //
 //  main.cpp
-//  Book Exercise 2.9
+//  Book Exercise 2.7
 //
-//  Created by ax on 8/29/16.
+//  Created by ax on 8/28/16.
 //  Copyright © 2016 COMP130. All rights reserved.
+//
+//  Physics: calculate acceleration of object
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main() {
+
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Calculate Acceleration\n";
+    
+    // Data
+    double v0 = 0.0;
+    double v1 = 0.0;
+    double t = 0.0;
+    double calc_acceleration = 0.0;
+    
+    // I
+    cout << "Enter v0, v1, and t: ";
+    cin >> v0 >> v1 >> t;
+    
+    // P
+    calc_acceleration = (v1 - v0) / t;
+    
+    // O
+    cout << "The average acceleration is " << calc_acceleration << endl;
+    
     return 0;
 }
